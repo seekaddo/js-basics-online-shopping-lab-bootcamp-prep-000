f468fd804031077b6ea7c451dfe1382e267ccfef
Dennis Addo <dennis_addo@aol.com>
2017-10-29 20:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ function viewCart() {
     console.log("Your shopping cart is empty.");
     return;
   }
-  
+
   var sent = "In your cart, you have ";
   var k = Object.keys(cart);
   for (var i = 0; i < k.length; i++) {
     if(cart.hasOwnProperty(k[i])){
-      sent += `{k[i]} at $${cart[k[i]]}${i === k.length-1?'.':', '}`;
+      sent += `{k[i]} at $${cart[k[i]]}${i === k.length-1?'.':${cart.length === 2 ? ' and ': ', '}}`;
     }
   }
   console.log(sent);
@@ -41,12 +41,44 @@ function viewCart() {
 
 function total() {
   // write your code here
+  //return Object.keys(cart).length;
+  var counter = 0;
+  for (var k in cart) {
+    if (cart.hasOwnProperty(k)) {
+      counter++; 
+     }
+  }
+  return counter;
 }
 
 function removeFromCart(item) {
   // write your code here
+  if (!cart.hasOwnProperty(item)) {
+    console.log("That item is not in your cart.");
+    return cart;
+  }
+
+  // for (var k in cart) {
+  //   if (cart.hasOwnProperty(k)) {
+  //       
+  //     }
+  // }
+  delete cart[item];
+  return cart;
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+
+  if (cardNumber === undefined ) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return;
+  }
+
+  console.log(`Your total cost is $${totalCost()}, which will be charged to the card ${cardNumber}.`);
+
+  for (const prop of Object.keys(cart)) {
+    delete cart[prop];
+  }
+  
 }
