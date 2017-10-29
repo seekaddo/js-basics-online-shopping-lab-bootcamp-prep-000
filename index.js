5cba3eb58d8a0fef1a5b1e311cbcaab30c15c6f0
Dennis Addo <dennis_addo@aol.com>
2017-10-29 20:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var cart = [];
 
+function getRandom() {
+    return Math.floor(Math.random() * 100);
+}
+
 function getCart() {
  return cart;
 }
@@ -10,11 +14,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  cart[item] = getRandom();
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.");
+    return;
+  }
+  
+  var sent = "In your cart, you have ";
+  var k = Object.keys(cart);
+  for (var i = 0; i < k.length; i++) {
+    if(cart.hasOwnProperty(k[i])){
+      sent += `{k[i]} at $${cart[k[i]]}${i === k.length-1?'.':', '}`;
+    }
+  }
+  console.log(sent);
+
 }
 
 function total() {
